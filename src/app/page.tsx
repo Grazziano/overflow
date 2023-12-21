0191c116c1294ba7835bbafb707de05e33b3983f
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2023-12-21 16:29:24 -0300

    Setup Mongo DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import { connectDB } from '@/config/db';
+import { currentUser } from '@clerk/nextjs';
+import { User } from '@clerk/nextjs/server';
+
+connectDB();
+
+export default async function Home() {
+  const user: User | null = await currentUser();
+
   return (
     <div>
       <h1>Homepage</h1>
+      <h1>Logged in user FirstName: {user?.firstName}</h1>
+      <h1>Logged in user LastName: {user?.lastName}</h1>
     </div>
   );
 }
